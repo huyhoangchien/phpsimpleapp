362d31281ec00c5267cc4a6ffadadbc7c137cf7c
Cloud User <centos@ip-172-31-30-47.us-west-2.compute.internal>
2018-03-18 06:32:50 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# php_simple_app
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: php_simple_app
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
